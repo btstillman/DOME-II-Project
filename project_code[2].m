aeea94e4b7f01559f5ff2ad8b65e6391777e349e
richlavallee <45393293+richlavallee@users.noreply.github.com>
2018-11-27 09:23:53 -0500

    Add files via upload

---
+++ project_code[2].m
@@ -0,0 +1,22 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% DOME II
+% Group Project 4-Stroke Cylinder
+% 11/20/18
+% This code returns a full dynamic analysis of the shaking forces as well
+% as the gas forces
+%%%%%%%%Variables%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% tht2_r , Theta_2 for the right stide of the engine
+% gamma2_r, gamma for the line of action to the y-axis for the right side.
+% r_g2r, link 2 for the right side.
+% R_G2r, link 2 vector for the right side.
+% A_G2r, acceleration for link 2 for the right side.
+% r_g3r, link 3 for the right side.
+% a_g3r, acceleration for link 3 for the right side.
+% omg2_r, the angular velocity for link 2 on the right side.
+% omg3_r, the angular velocity for link 3 on the right side.
+% l_r, the length of link 3 or the connecting rod.
+% d_r,
+% c_r,
+% d_ddr, 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+function 
